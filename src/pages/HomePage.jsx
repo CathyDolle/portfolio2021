18063876c77a0f2c4563345d48d728015d3f4c24
CathyDolle <cathy.dolle@live.fr>
2020-11-28 18:28:26 +0100

    Homepage edit
    Pixel perfect the text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Wrapper from './../components/Wrapper'
 import Arkestar from './../assets/img/works/arkestar.png'
 
 
-//TYPO
+//TYPOGRAPHY
 
 // DOLLE
 const LastName = styled.h1`
@@ -13,38 +13,50 @@ const LastName = styled.h1`
   font-weight: 500;
   font-style: normal;
   font-size: 11.8rem;
-  margin: 0 20px 0 -5px;
+  margin: 0 2rem 0 -0.5rem;
   color: ${props => props.theme.colors.fontColor};
 `;
 
 // Cathy
 const FirstName = styled.h1`
   font-family: 'Saol Display';
-  margin: 0 0 -28px 0;
+  margin: 0 0 -2.8rem 0;
   font-size: 11.8rem;
   color: ${props => props.theme.colors.fontColor};
 `;
 
+// UI UX
 const Skill = styled.h3`
   font-family: neue-haas-grotesk-text, sans-serif;
   font-weight: 400;
-  margin: -10px 20px 0 0;
+  margin: -1rem 2rem 0 0;
   font-style: normal;
   font-size: 4rem;
   color: ${props => props.theme.colors.fontColor};
 `;
 
+// DEV
 const Skill2 = styled.h3`
   font-family: neue-haas-grotesk-text, sans-serif;
   font-weight: 400;
-  margin-top: 4px;
+  margin-top: 0.4rem;
   font-style: normal;
   font-size: 1.8rem;
   color: ${props => props.theme.colors.fontColor};
 `;
 
-//Setions
+// JAPANESE CHAR 
+const JapaneseText = styled.span`
+  font-family: neue-haas-grotesk-text, sans-serif;
+  color: ${props => props.theme.colors.mainColor};
+  font-size: 10rem;
+  margin: -4rem 0 0 3.0rem;
+  font-weight: 600;
+`;
 
+// SECTIONS
+
+// GLOBAL TEXT SECTION
 const TextContent = styled.section`
   width: ${props => props.theme.sizes.threeColumns};
   height: 100%;
@@ -55,32 +67,31 @@ const TextContent = styled.section`
   flex-direction: column;
 `;
 
+
+// TEXT NAME
 const TextName = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 10px;
 `;
 
+// Description (Skills)
 const DescriptionContent = styled.div`
   display: flex;
 `
 
+// Description section
 const TextDescription = styled.div`
   display: flex;
   flex-direction: column;
 `
 
+// SKILL Section
 const TextDescriptionSkill = styled.div`
   position: relative;
   display: flex;
 `;
 
-const JapaneseText = styled.span`
-  font-family: neue-haas-grotesk-text, sans-serif;
-  color: ${props => props.theme.colors.mainColor};
-  font-size: 10rem;
-  margin: -45px 0 0 30px;
-  font-weight: 600;
-`;
 
 //IMG
 
